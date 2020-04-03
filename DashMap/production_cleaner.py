be3b8548a391b4ee1de5d9b759726e9be07a3474
craig <craigmariani47@gmail.com>
2020-04-02 17:23:45 -0700

    updates

--- DashMap/production_cleaner.py
+++ DashMap/production_cleaner.py
@@ -1,24 +1,36 @@
 # drop duplicate values and null values and create address
 import pandas as pd
 import numpy as np
+import base64
+import datetime
+import io
+import plotly.graph_objs as go
+# import cufflinks as cf
+
+import dash
+from dash.dependencies import Input, Output, State
+import dash_core_components as dcc
+import dash_html_components as html
+import dash_table
 
 class Cleaner:
+
 
 	# drops duplicates and null values, works for all data sets
 	def full_cleaner(df):
 		df = df.dropna() # drop null values
 		df = df.drop_duplicates() # drop duplicate records
 
-		# delete records from other countrues that are not United States
+		# delete records from other countries that are not United States
 		indexNames = df[ df['Country'] != 'United States of America'].index
 		df.drop(indexNames, inplace=True)
 
-		df = df.drop(columns='Index')
+		# df = df.drop(columns='Index')
 
 		df['Index'] = np.arange(df.shape[0])
 
 		return df
-		
+			
 	# creates new attributes like Mnths Passed and Address, needs StartDate/EndDate and City, State, Country
 	def feature_engineering(df):
 		
@@ -36,18 +48,41 @@ class Cleaner:
 			# df['ProjectDurationInDays'] = df['ProjectDurationInDays'].astype('datetime64').astype(int).astype(float)
 			# print(splitdays)
 			# df['ProjectDurationInDays'] = pd.to_datetime64(df['ProjectDurationInDays'])
+			# print('Project Duration')
+			# print(df['ProjectDurationInDays'])
 		except KeyError:
 			print('No date-time values given for feature "TimePassed"')
 
 		try:
 
-			df['Address'] = df['City'] + ', ' + df['State'] + ', ' + df['Country']
+			# df['Address'] = df['City'] + ', ' + df['State'] + ', ' + df['Country']
+			df['Address'] = df['City'] + '-' + df['State'] + '-' + df['Country']
 
 		except KeyError:
 			print('Not enough geospatial data given for feature "Address"')
 
+		
 
-		df.to_csv('../../Data/ProductionData/cleaned_geospatialdata.csv')
+		try:
+			zips = df['PostalCode'].tolist()
+			Zip_Codes = []
+
+			for code in zips:
+				code_num = code.split('-')
+
+				Zip_Codes.append(code_num[0])
+
+			df = df.drop(columns='PostalCode')
+			df['PostalCode'] = Zip_Codes
+			df['PostalCode'] = pd.to_numeric(df['PostalCode'])
+
+		except KeyError:
+			print('Not enough geospatial data given to format feature "PostalCode" ')
+
+		# print(df['PostalCode'])
+		
+		
+		# df.to_csv('../../Data/ProductionData/cleaned_geospatialdata.csv')
 		return df
 
 
